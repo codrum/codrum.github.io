49089411ecb7bfbe7c72a9550dc5d5ee68c75f79
codrum <landrum287@hotmail.com>
2022-08-23 22:44:40 -0500

    change url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
 }
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+	assetPrefix: isProd ? '/stillwaterdiscgolf.org/' : '',
+	nextConfig,
+}
