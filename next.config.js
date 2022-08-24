4de2af71e8a8d52f70f0d001daf8a63ffd2eb289
codrum <landrum287@hotmail.com>
2022-08-23 23:11:07 -0500

    config fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: { unoptimized: true },
 	assetPrefix: isProd ? '/codrum.github.io/' : '',
 }
-
-const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = nextConfig
