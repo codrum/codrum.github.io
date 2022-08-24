5893ac6545ee54475c3ac4963c7cc509c3e830e6
codrum <landrum287@hotmail.com>
2022-08-23 23:29:03 -0500

    fix custom domain

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	assetPrefix: isProd ? '/codrum.github.io/' : '',
+	assetPrefix: isProd ? '/stillwaterdiscgolf.org/' : '',
 }
 
 module.exports = {
