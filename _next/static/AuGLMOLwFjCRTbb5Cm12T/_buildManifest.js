66b10077f8dd931fd1adad9efcea694a7af39d04
codrum <codrum@users.noreply.github.com>
2022-11-11 19:50:06 +0000

    deploy: fb519619339e0e401323a7cf408a2c795a4b3147

--- _next/static/AuGLMOLwFjCRTbb5Cm12T/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/619-bfa898760d0e189a.js","static/chunks/pages/index-3779560751217483.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/fDSeQWkNYxkQBThp7HkhE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/619-bfa898760d0e189a.js","static/chunks/pages/index-44d2f9b68b2f9742.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
