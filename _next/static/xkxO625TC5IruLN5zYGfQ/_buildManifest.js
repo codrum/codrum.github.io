f8dd797ceb3dee5340d20f0d9b13e661e1a634d1
codrum <codrum@users.noreply.github.com>
2022-11-17 02:16:21 +0000

    deploy: b363e11d8cab6081df8970077f78d3b1ccf6a286

---
+++ _next/static/xkxO625TC5IruLN5zYGfQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/809-a1e5064c6602c553.js","static/chunks/pages/index-e4e3404a79e887a7.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
